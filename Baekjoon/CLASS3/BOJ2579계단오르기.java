af64c4a2aa3658facb01f2e3c0b7f4ef770c1a21
csjcozy <tmdwns63@gmail.com>
2024-09-26 21:25:55 +0900

    update: dp-계단오르기

--- Baekjoon/CLASS3/BOJ2579계단오르기.java
+++ Baekjoon/CLASS3/BOJ2579계단오르기.java
@@ -3,11 +3,38 @@ package Baekjoon.CLASS3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class BOJ2579계단오르기 {
+
+    static Integer[] dp;
+    static int[] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+
+        dp = new Integer[n + 1];
+        arr = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        dp[0] = arr[0];
+        dp[1] = arr[1];
+
+        if (n >= 2) {
+            dp[2] = arr[1] + arr[2];
+        }
+
+        System.out.println(find(n));
+    }
+
+    public static int find(int n) {
+        if (dp[n] == null) {
+            dp[n] = Math.max(find(n - 2), find(n - 3) + arr[n - 1]) + arr[n];
+        }
+
+        return dp[n];
     }
 }
