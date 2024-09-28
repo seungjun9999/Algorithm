4333e12c5c78c28235603a828f3756a8e3327141
csjcozy <tmdwns63@gmail.com>
2024-09-29 01:09:00 +0900

    update: hashmap-패션왕신해빈

---
+++ Baekjoon/CLASS3/BOJ9375패션왕신해빈.java
@@ -0,0 +1,31 @@
+package Baekjoon.CLASS3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class BOJ9375패션왕신해빈 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        HashMap<String, Integer> hm;
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            hm = new HashMap<>();
+            int m = Integer.parseInt(br.readLine());
+            int result = 1;
+            for (int j = 0; j < m; j++) {
+                st = new StringTokenizer(br.readLine());
+                st.nextToken();
+                String b = st.nextToken();
+                hm.put(b, hm.getOrDefault(b, 0) + 1);
+            }
+            for (int val : hm.values()) {
+                result *= val + 1;
+            }
+            System.out.println(result - 1);
+        }
+    }
+}
