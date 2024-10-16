714839065eb312fc32b46693f778098297d2a2eb
csjcozy <117347226+csjcozy@users.noreply.github.com>
2024-10-16 23:32:54 +0900

    update: BOJ Algo 2138전구.java 현재 메모리 초과

---
+++ Baekjoon/분류/BOJ2138전구와스위치.java
@@ -0,0 +1,74 @@
+import java.io.*;
+
+public class BOJ2138전구와스위치 {
+    static int n, cnt1 = 0, cnt2 = 1;
+    static String arr, first, second;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        char[] sw1 = new char[n];
+        char[] sw2 = new char[n];
+
+        for (int i = 0; i < 2; i++) {
+            arr = br.readLine();
+            if (i == 0) {
+                for (int j = 0; j < n; j++) {
+                    sw1[j] = arr.charAt(j);
+                    sw2[j] = arr.charAt(j);
+                }
+            }
+        }
+
+        for (int i = 0; i < 2; i++) {
+            if (sw2[i] == '0') {
+                sw2[i] = '1';
+            } else {
+                sw2[i] = '0';
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            if (sw1[i - 1] != arr.charAt(i - 1)) {
+                change(sw1, i - 1);
+                cnt1++;
+            }
+            if (sw2[i - 1] != arr.charAt(i - 1)) {
+                change(sw2, i - 1);
+                cnt2++;
+            }
+            if (check(sw1, sw2)) {
+                if (first.equals(second)) {
+                    System.out.println(Math.min(cnt1, cnt2));
+                } else if (first.equals(arr)) {
+                    System.out.println(cnt1);
+                } else {
+                    System.out.println(cnt2);
+                }
+                return;
+            }
+        }
+        System.out.println(-1);
+    }
+
+    static void change(char[] sw, int a) {
+        for (int i = a; i < a + 3; i++) {
+            if (i < n) {
+                if (sw[i] == '0') {
+                    sw[i] = '1';
+                } else {
+                    sw[i] = '0';
+                }
+            }
+        }
+    }
+
+    static boolean check(char[] sw1, char[] sw2) {
+        first = String.valueOf(sw1);
+        second = String.valueOf(sw2);
+        if (!first.equals(arr) && !second.equals(arr)) {
+            return false;
+        }
+        return true;
+    }
+}
