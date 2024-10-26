0fc2803c3fa2bd47593449691e428341ca3294cf
csjcozy <117347226+csjcozy@users.noreply.github.com>
2024-10-26 18:51:11 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 1784 ms, Memory: 72888 KB -BaekjoonHub

---
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int[][] num;
+    static int[][] result;
+    static Queue<int[]> q = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        num = new int[n][m];
+        result = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                num[i][j] = Integer.parseInt(st.nextToken());
+                if (num[i][j] == 2) {
+                    q.offer(new int[]{i, j});
+                }
+                if (num[i][j] == 1) {
+                    result[i][j] = -1;
+                }
+            }
+        }
+        bfs();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void bfs() {
+        while (!q.isEmpty()) {
+            int[] where = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int x = where[0] + dx[i];
+                int y = where[1] + dy[i];
+                if (check(x, y) && num[x][y] == 1) {
+                    num[x][y] = 0;
+                    result[x][y] = result[where[0]][where[1]] + 1;
+                    q.offer(new int[]{x, y});
+                }
+            }
+        }
+    }
+
+    static boolean check(int x, int y) {
+        if (x >= 0 && x < n && y >= 0 && y < m) {
+            return true;
+        }
+        return false;
+    }
+}
