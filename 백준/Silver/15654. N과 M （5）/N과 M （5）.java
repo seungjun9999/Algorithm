dcb36cc45bc7d830e329b938f86b70e960a8207d
csjcozy <117347226+csjcozy@users.noreply.github.com>
2024-11-20 00:27:41 +0900

    [Silver III] Title: N과 M (5), Time: 320 ms, Memory: 47744 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static int[] num;
+    static boolean[] visited;
+    static StringBuilder sb;
+    static ArrayList<Integer> list = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        num = new int[n];
+        visited = new boolean[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(num);
+
+        dfs(0, 0);
+        System.out.print(sb);
+    }
+
+    static void dfs(int a, int cnt) {
+        if (cnt == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(list.get(i)).append(" ");
+
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = a; i < n; i++) {
+            if(!visited[i]){
+                list.add(num[i]);
+                visited[i] = true;
+                dfs(a, cnt + 1);
+                list.remove(list.size() - 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
