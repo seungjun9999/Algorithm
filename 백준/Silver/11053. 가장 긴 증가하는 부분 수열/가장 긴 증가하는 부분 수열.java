5686a806f441914d13111e5602cdf6777946a17d
csjcozy <117347226+csjcozy@users.noreply.github.com>
2024-11-21 00:23:31 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 116 ms, Memory: 14512 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -2,25 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int n;
+    static ArrayList<Integer> list = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         int[] num = new int[n];
-        int[] dp = new int[n];
-        int result = 1;
         for (int i = 0; i < n; i++) {
             num[i] = Integer.parseInt(st.nextToken());
-            dp[i] = 1;
         }
+        list.add(num[0]);
         for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (num[i] > num[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                    result = Math.max(result,dp[i]);
-                }
+            if(list.get(list.size()-1) < num[i] ){
+                list.add(num[i]);
+            }else {
+                list.set(bs(num[i]),num[i]);
             }
         }
-        System.out.println(result);
+        System.out.println(list.size());
+    }
+
+    static int bs(int t){
+        int start = 0;
+        int end = list.size();
+        int result = 0;
+        while(start<end){
+            int mid = (start + end) /2;
+            if(list.get(mid) < t){
+                start = mid+1;
+            }else{
+                end = mid;
+                result = mid;
+            }
+        }
+        return result;
     }
 }
