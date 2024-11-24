c833b030908ff909549343fc3695a144195796d4
csjcozy <117347226+csjcozy@users.noreply.github.com>
2024-11-25 00:03:14 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1088 ms, Memory: 66348 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+    static ArrayDeque<Integer> q = new ArrayDeque<>();
+    static boolean[] visited;
+    static int[] num;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        visited = new boolean[n + 1];
+        num = new int[n + 1];
+        StringTokenizer st;
+        for (int i = 0; i < n + 1; i++) {
+            list.add(new ArrayList<>());
+        }
+        for (int i = 1; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            list.get(a).add(b);
+            list.get(b).add(a);
+        }
+        q.offer(1);
+        visited[1] = true;
+        bfs();
+        for (int i = 2; i <= n; i++) {
+            System.out.println(num[i]);
+        }
+    }
+
+    static void bfs() {
+        while (!q.isEmpty()) {
+            int t = q.poll();
+            for (int i = 0; i < list.get(t).size(); i++) {
+                int now = list.get(t).get(i);
+                if (!visited[now]) {
+                    num[now] = t;
+                    visited[now] = true;
+                    q.offer(now);
+                }
+            }
+        }
+    }
+}
