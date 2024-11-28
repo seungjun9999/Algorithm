88d0dad389f58df4c2870edadf3e9bdee2a832d6
csjcozy <117347226+csjcozy@users.noreply.github.com>
2024-11-28 19:43:32 +0900

    [Silver II] Title: A → B, Time: 108 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -2,44 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static long a, b, result;
-    static ArrayDeque<Node> adq = new ArrayDeque<>();
-
-    static class Node {
-        long x, cnt;
-
-        Node(long x, long cnt) {
-            this.x = x;
-            this.cnt = cnt;
-        }
-    }
-
+    static int a, b, result = 1;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         a = Integer.parseInt(st.nextToken());
         b = Integer.parseInt(st.nextToken());
-        adq.offer(new Node(a, 1));
-        bfs();
-        System.out.println(result);
-    }
-
-    static void bfs() {
-        while (!adq.isEmpty()) {
-            Node now = adq.poll();
-            if (now.x==b) {
-                result = now.cnt;
-                return;
+        while (b != a) {
+            if (b < a || (b % 2 != 0 && b % 10 != 1)) {
+                result = -1;
+                break;
             }
-            long q = now.x * 2;
-            long w = now.x * 10 + 1;
-            if (q <= b) {
-                adq.offer(new Node(q, now.cnt + 1));
+            if (b % 2 == 0) {
+                b /= 2;
+            } else {
+                b /= 10;
             }
-            if (w <= b) {
-                adq.offer(new Node(w, now.cnt + 1));
-            }
+            result++;
         }
-        result=-1;
+        System.out.println(result);
     }
 }
