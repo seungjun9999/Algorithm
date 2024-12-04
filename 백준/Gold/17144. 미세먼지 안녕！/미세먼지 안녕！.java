1bbd4fb88faf199ba3d5b8fe87e2f6830f9e24e2
csjcozy <117347226+csjcozy@users.noreply.github.com>
2024-12-04 16:31:21 +0900

    미세먼지 안녕!

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -4,9 +4,10 @@ import java.util.*;
 public class Main {
     static int r, c, t, airup = 0, airdw = 0, result = 0;
     static int[][] num, map;
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, 1, 0, -1};
+    static int[] dx = { 1, 0, -1, 0 };
+    static int[] dy = { 0, 1, 0, -1 };
 
+    // 완전 구현 문제
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
