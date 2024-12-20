fed3c98c513e1327b5ce4ed62373ea0aeae13ed0
csjcozy <117347226+csjcozy@users.noreply.github.com>
2024-12-20 20:25:49 +0900

    [Silver I] Title: 포도주 시식, Time: 124 ms, Memory: 14956 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] num, dp;
+        num = new int[n + 1];
+        dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            num[i] = Integer.parseInt(br.readLine());
+        }
+        dp[1] = num[1];
+        int result = dp[1];
+        if (n > 1) {
+            dp[2] = num[1] + num[2];
+            result = dp[2];
+        }
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(Math.max(dp[i - 3] + num[i - 1], dp[i - 2]) + num[i], dp[i - 1]);
+            result = Math.max(dp[i], result);
+        }
+        System.out.println(result);
+    }
+}
