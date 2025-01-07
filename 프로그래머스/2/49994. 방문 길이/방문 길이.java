6c5db1dd3e5842db49753f5fe85217873df969f0
csjcozy <117347226+csjcozy@users.noreply.github.com>
2025-01-07 17:45:16 +0900

    [level 2] Title: 방문 길이, Time: 27.68 ms, Memory: 89.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String dirs) {
+        int answer = 0;
+        int[][]num = new int[11][11];
+        HashSet<String> hs = new HashSet<>();
+        int[] my = {5,5};
+        for(int i=0;i<dirs.length();i++){
+            char go = dirs.charAt(i);
+            if(go=='U'){
+                int recent = my[0]+1;
+                if(check(recent)){
+                    hs.add(my[0]+""+my[1]+""+recent+""+my[1]);
+                    hs.add(recent+""+my[1]+""+my[0]+""+my[1]);
+                    my = new int[]{recent,my[1]};
+                }
+            }else if(go=='D'){
+                int recent = my[0]-1;
+                if(check(recent) ){
+                    hs.add(my[0]+""+my[1]+""+recent+""+my[1]);
+                    hs.add(recent+""+my[1]+""+my[0]+""+my[1]);
+                    my = new int[]{recent,my[1]};
+                }
+            }else if(go=='R'){
+                int recent = my[1]+1;
+                if(check(recent)){
+                    hs.add(my[0]+""+my[1]+""+my[0]+""+recent);
+                    hs.add(my[0]+""+recent+""+my[0]+""+my[1]);
+                    my = new int[]{my[0],recent};
+                }
+            }else{
+                int recent = my[1]-1;
+                if(check(recent)){
+                    hs.add(my[0]+""+my[1]+""+my[0]+""+recent);
+                    hs.add(my[0]+""+recent+""+my[0]+""+my[1]); 
+                    my = new int[]{my[0],recent};
+                }
+            }
+        }
+        return hs.size()/2;
+    }
+    static boolean check(int a){
+        if(0<=a && a<11){
+            return true;
+        }
+        return false;
+    }
+}
