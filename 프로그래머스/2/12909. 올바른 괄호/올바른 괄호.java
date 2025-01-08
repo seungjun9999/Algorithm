4217bfa65998ebe56af2c67515639844592e7103
SJ Choi <117347226+seungjun9999@users.noreply.github.com>
2025-01-08 16:44:45 +0900

    [level 2] Title: 올바른 괄호, Time: 25.05 ms, Memory: 55.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,26 +1,25 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
     boolean solution(String s) {
         Deque<Character> dq = new ArrayDeque<>();
-        int cnt=0;
-        
-        for(int i=0; i<s.length();i++){
+        int cnt = 0;
+
+        for (int i = 0; i < s.length(); i++) {
             dq.push(s.charAt(i));
         }
-        while(!dq.isEmpty()){
+        while (!dq.isEmpty()) {
             char c = dq.pop();
-            if(c == ')'){
-                cnt+=1;
-            }else{
-                cnt-=1;
+            if (c == ')') {
+                cnt += 1;
+            } else {
+                cnt -= 1;
             }
-            if(cnt<0){
+            if (cnt < 0) {
                 return false;
             }
         }
-        if(cnt!=0){
+        if (cnt != 0) {
             return false;
         }
 
