b4c5b77cc7d75106192a7790dc0285f1f12790fe
SJ Choi <117347226+seungjun9999@users.noreply.github.com>
2025-01-09 15:52:08 +0900

    [level 2] Title: 괄호 회전하기, Time: 32.38 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -4,31 +4,31 @@ class Solution {
     public int solution(String s) {
         int[] gual = new int[3];
         int result=0;
-        for(int i=0;i<s.length();i++){
-            char c = s.charAt(i);
-            if('[' == c){
-                gual[0]++;
-            }else if(c==']'){
-                gual[0]--;
-            }else if(c=='{'){
-                gual[1]++;
-            }else if(c=='}'){
-                gual[1]--;
-            }else if(c=='('){
-                gual[2]++;
-            }else{
-                gual[2]--;
-            }
-        }
+        // for(int i=0;i<s.length();i++){
+        //     char c = s.charAt(i);
+        //     if('[' == c){
+        //         gual[0]++;
+        //     }else if(c==']'){
+        //         gual[0]--;
+        //     }else if(c=='{'){
+        //         gual[1]++;
+        //     }else if(c=='}'){
+        //         gual[1]--;
+        //     }else if(c=='('){
+        //         gual[2]++;
+        //     }else{
+        //         gual[2]--;
+        //     }
+        // }
         for(int i=0;i<3;i++){
             if(gual[i]!=0){
                 System.out.println("여기??");
                 return 0;
             }
         }
-
+        ArrayDeque<Character> dq = new ArrayDeque<>();
         for(int i=0;i<s.length();i++){
-             ArrayDeque<Character>dq = new ArrayDeque<>();
+            dq = new ArrayDeque<>();
             for(int j=i;j<s.length()+i;j++){
                 char c = s.charAt(j%s.length());
                 if(c=='[' || c=='{'|| c=='('){
@@ -47,33 +47,12 @@ class Solution {
                         }
                     }
                 }
-                
-                // if('[' == c){
-                //     gual[0]++;
-                // }else if(c==']'){
-                //     if(--gual[0]<0){
-                //         result--;
-                //         break;
-                //     }
-                // }else if(c=='{'){
-                //     gual[1]++;
-                // }else if(c=='}'){
-                //     if(--gual[1]<0){
-                //         result--;
-                //         break;
-                //     }
-                // }else if(c=='('){
-                //     gual[2]++;
-                // }else{
-                //     if(--gual[2]<0){
-                //         result--;
-                //         break;
-                //     }
-                // }
             }
             result++;
         }
-
+        if(!dq.isEmpty()){
+            return 0;
+        }
         return result;
     }
 }
