014acc3b18734d4b1d2b888b04fc9a835ea2082c
SJ Choi <117347226+seungjun9999@users.noreply.github.com>
2025-01-11 19:42:15 +0900

    [level 2] Title: 기능개발, Time: 8.68 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586#) 
 
 ### 성능 요약
 
-메모리: 83.3 MB, 시간: 0.98 ms
+메모리: 70.6 MB, 시간: 8.68 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 11일 19:23:48
+2025년 01월 11일 19:42:12
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,39 +1,26 @@
 import java.util.*;
 
 class Solution {
-    class Node{
-        int pro,spd;
-        
-        Node(int pro,int spd){
-            this.pro = pro;
-            this.spd = spd;
-        }
-    }
+
     public int[] solution(int[] progresses, int[] speeds) {
-        ArrayDeque<Node> dq = new ArrayDeque<>();
-        ArrayDeque<Integer> num = new ArrayDeque<>();
-        for(int i = 0; i < progresses.length;i++){
-            dq.offer(new Node(progresses[i],speeds[i]));
-        }
-        while(!dq.isEmpty()) {
-            int cnt=1;
-            Node recent = dq.poll();
-            int d = (int) Math.ceil((100-recent.pro) / (double) recent.spd);
-            while(!dq.isEmpty()) {
-                Node now =  dq.peek();
-                if(now.pro + now.spd*d >= 100){
-                    dq.poll();
-                    cnt++;
-                }else {
-                    break;
-                }
+         ArrayDeque<Integer> num = new ArrayDeque<>();
+        int cnt = 0;
+        int day = 0;
+        for (int i = 0; i < progresses.length; i++) {
+            int recent = (int) Math.ceil((100 - progresses[i]) / (double) speeds[i]);
+            if (i == 0) {
+                day = recent;
+                cnt++;
+            } else if (day >= recent) {
+                cnt++;
+            } else {
+                num.offer(cnt);
+                day = recent;
+                cnt = 1;
             }
-            num.offer(cnt);
         }
-        int[] answer = new int[num.size()];
-        for(int i=0;i<answer.length;i++){
-            answer[i] = num.poll();
-        }
-        return answer;
+        num.offer(cnt);
+        
+        return num.stream().mapToInt(i -> i).toArray();
     }
 }
