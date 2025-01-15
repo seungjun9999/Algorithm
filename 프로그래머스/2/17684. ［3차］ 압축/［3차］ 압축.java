cebed5f89dbdd95e97695e82e7d2d5f0329700f4
SJ Choi <117347226+seungjun9999@users.noreply.github.com>
2025-01-15 19:35:38 +0900

    [level 2] Title: [3차] 압축, Time: 48.09 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String msg) {
+        HashMap<String,Integer> hm = new HashMap<>();
+        ArrayList<Integer> answer = new ArrayList<>();
+        int idx = 26;
+        for (int i=0;i<idx;i++) {
+            hm.put(String.valueOf((char)('A'+i)),1+i);
+        }
+        int a=0;
+        String arr = "";
+        while (msg.length() > a) {
+            arr += msg.charAt(a);
+            if( (a+1)<msg.length() && !hm.containsKey(arr+msg.charAt(a+1))) {
+                hm.put(arr+msg.charAt(a+1),++idx);
+                answer.add(hm.get(arr));
+                arr = "";
+            }
+            a++;
+        }
+        answer.add(hm.get(arr));
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
