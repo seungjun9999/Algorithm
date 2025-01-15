0e9178485fb58767d39537580adc5248b19a3b91
SJ Choi <117347226+seungjun9999@users.noreply.github.com>
2025-01-15 18:02:54 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 35.10 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,44 +1,43 @@
 import java.util.*;
 
 class Solution {
-    static HashMap<String,Integer> combi;
+    static HashMap<String, Integer> combi;
     static ArrayList<String> answer = new ArrayList<>();
-    
+
     public String[] solution(String[] orders, int[] course) {
-        a: for(int len : course) {
+        a:
+        for (int len : course) {
             combi = new HashMap<>();
-            for(String order : orders) {
-                if(order.length() >= len) {
-                    combination(order,len,"",0);
-                }  
+            for (String order : orders) {
+                if (order.length() >= len) {
+                    combination(order, len, "", 0);
+                }
             }
-            if(combi.isEmpty()) continue a;
+            if (combi.isEmpty()) continue a;
             int maax = Collections.max(combi.values());
-            if(maax > 1) {
-                for(String key : combi.keySet()) {
-                    if(combi.get(key) == maax) {
+            if (maax > 1) {
+                for (String key : combi.keySet()) {
+                    if (combi.get(key) == maax) {
                         answer.add(key);
                     }
                 }
             }
         }
-        Collections.sort(answer);
-        
-        return answer.toArray(new String[0]);
+        return answer.stream().sorted().toArray(String[]::new);
     }
-    
-    void combination(String order, int len, String current,int idx) {
-        
-        if(current.length() == len) {
+
+    void combination(String order, int len, String current, int idx) {
+
+        if (current.length() == len) {
             char[] arrchar = current.toCharArray();
             Arrays.sort(arrchar);
             String key = new String(arrchar);
-            combi.put(key,combi.getOrDefault(key,0)+1);
+            combi.put(key, combi.getOrDefault(key, 0) + 1);
             return;
         }
-        
-        for(int i=idx;i<order.length();i++) {
-            combination(order,len,current+order.charAt(i),i+1);
+
+        for (int i = idx; i < order.length(); i++) {
+            combination(order, len, current + order.charAt(i), i + 1);
         }
     }
 }
