ecbfd4313639b2b0c6dd8c471f35a53f015d6aa9
SJ Choi <117347226+seungjun9999@users.noreply.github.com>
2025-01-16 17:55:04 +0900

    [level 2] Title: 예상 대진표, Time: 1.42 ms, Memory: 98.9 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -3,11 +3,15 @@ class Solution
     public int solution(int n, int a, int b)
     {
         int answer=1;
-        while(Math.abs(b-a)!=1 || (int)Math.ceil((double)b/2) != (int)Math.ceil((double)a/2)) {
-            b= (int)Math.ceil((double)b/2);
-            a= (int)Math.ceil((double)a/2);
+        while(Math.abs(b-a)!=1 || ceil(a) != ceil(b)) {
+            b = ceil(b);
+            a = ceil(a);
             answer++;
         }
         return answer;
     }
+    
+    int ceil(int x){
+        return (int)Math.ceil((double)x/2);
+    } 
 }
