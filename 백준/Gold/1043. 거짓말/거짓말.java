a58baf54e60035ab0976a06ebb18754b4646810e
SJ Choi <117347226+seungjun9999@users.noreply.github.com>
2025-01-17 14:08:58 +0900

    [Gold IV] Title: 거짓말, Time: 108 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -1,72 +1,58 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main
-{
-    static ArrayList<ArrayList<Integer>> listpt = new ArrayList<>();
-    static ArrayList<ArrayList<Integer>> listps = new ArrayList<>();
-    static boolean[] person;
-    static boolean[] visited;
-    static int n,m,cnt;
-    static Queue<Integer> q = new LinkedList<>();
-
-    public static void main(String args[]) throws IOException
-    {
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        cnt = m;
-        visited = new boolean[m];
-        person = new boolean[n+1];
-
-        for(int i = 0; i < n+1 ; i++){
-            listps.add(new ArrayList<Integer>());
-        }
-        for(int i = 0; i < m ; i++){
-            listpt.add(new ArrayList<Integer>());
-        }
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        int k = Integer.parseInt(st.nextToken());
-        for(int i = 0; i < k; i++){
+        int trues = Integer.parseInt(st.nextToken());
+        boolean[] visited = new boolean[n+1];
+        ArrayDeque<Integer> dq = new ArrayDeque<>();
+        HashSet<Integer> party = new HashSet<>();
+
+
+
+        for (int i = 0; i < trues; i++) {
             int a = Integer.parseInt(st.nextToken());
-            q.offer(a);
-            person[a] = true;
+            dq.offer(a);
+            visited[a] = true;
         }
 
-        for(int i = 0; i < m;i++){
+        HashMap<Integer, ArrayList<Integer>> people = new HashMap<>();
+        HashMap<Integer,ArrayList<Integer>> partys = new HashMap<>();
+
+        for(int i=1;i<=n;i++) {
+            people.put(i,new ArrayList<>());
+        }
+
+        for (int i = 1; i <= m; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            for(int j = 0; j < a; j++){
-                int b = Integer.parseInt(st.nextToken());
-                listps.get(b).add(i);
-                listpt.get(i).add(b);
+            int k = Integer.parseInt(st.nextToken());
+            partys.put(i, new ArrayList<>());
+            for(int j=0;j<k;j++) {
+                int a = Integer.parseInt(st.nextToken());
+                partys.get(i).add(a);
+                people.get(a).add(i);
             }
         }
 
-        bfs();
-        System.out.println(cnt);
-    }
-
-    static void bfs(){
-        while(!q.isEmpty()){
-            int now = q.poll();
-            for(int i = 0; i < listps.get(now).size(); i++){
-                int pt = listps.get(now).get(i);
-                if(!visited[pt]){
-                    visited[pt] = true;
-                    cnt--;
-                    for(int j = 0; j< listpt.get(pt).size(); j++){
-                        int recent = listpt.get(pt).get(j);
-                        if(!person[recent]){
-                            q.offer(recent);
-                            person[recent] = true;
-                        }
+        while (!dq.isEmpty()) {
+            ArrayList<Integer> whatparty = new ArrayList<>(people.get(dq.poll()));
+            for (int b : whatparty) {
+                party.add(b);
+                ArrayList<Integer> whopeople = new ArrayList<>(partys.get(b));
+                for(int c: whopeople) {
+                    if(!visited[c]) {
+                        dq.offer(c);
+                        visited[c] = true;
                     }
                 }
             }
         }
+        System.out.println(m - party.size());
     }
 }
