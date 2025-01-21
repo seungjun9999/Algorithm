62ed541dbbe9b316158e249ebdc223861365edd1
SJ Choi <117347226+seungjun9999@users.noreply.github.com>
2025-01-21 16:27:06 +0900

    [level 3] Title: 양과 늑대, Time: 17.98 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -3,31 +3,32 @@ import java.util.*;
 class Solution {
     static ArrayList<ArrayList<Integer>> list = new ArrayList<>();
     static int result = 0;
+
     public int solution(int[] info, int[][] edges) {
-        for(int i=0;i<info.length;i++) {
+        for (int i = 0; i < info.length; i++) {
             list.add(new ArrayList<>());
         }
-        for(int[] edge: edges) {
+        for (int[] edge : edges) {
             list.get(edge[0]).add(edge[1]);
         }
-        bfs(0,0,0,new HashSet<Integer>(),info);
+        bfs(0, 0, 0, new HashSet<Integer>(), info);
         return result;
     }
-    
-    void bfs(int recent, int sheep, int wolf, HashSet<Integer> set,int[] info) {
-        if(info[recent]==0) {
-            result = Math.max(result,++sheep);
-        }else{
-            wolf+=1;
+
+    void bfs(int recent, int sheep, int wolf, HashSet<Integer> set, int[] info) {
+        if (info[recent] == 0) {
+            result = Math.max(result, ++sheep);
+        } else {
+            wolf += 1;
         }
-        if(sheep<=wolf){
+        if (sheep <= wolf) {
             return;
         }
         set.addAll(list.get(recent));
-        for(int a : set) {
+        for (int a : set) {
             HashSet<Integer> visited = new HashSet<>(set);
             visited.remove(a);
-            bfs(a,sheep,wolf,visited,info);
-        } 
+            bfs(a, sheep, wolf, visited, info);
+        }
     }
 }
