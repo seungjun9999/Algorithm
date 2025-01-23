7ee6479a706a68a64664897c9bf91aa3db286a68
SJ Choi <117347226+seungjun9999@users.noreply.github.com>
2025-01-23 18:34:51 +0900

    [level 2] Title: 미로 탈출, Time: 7.33 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/README.md
+++ 프로그래머스/2/159993. 미로 탈출/README.md
@@ -1,10 +1,10 @@
 # [level 2] 미로 탈출 - 159993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993#) 
 
 ### 성능 요약
 
-메모리: 94.6 MB, 시간: 64.30 ms
+메모리: 85.5 MB, 시간: 7.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 23일 18:29:11
+2025년 01월 23일 18:34:56
 
 ### 문제 설명
 
--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -2,58 +2,54 @@ import java.util.*;
 
 class Solution {
     static char[][] map;
-    static int[] dx = {1,0,-1,0};
-    static int[] dy = {0,1,0,-1};
-    static boolean[][] visited;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
     static int[][] num;
-    static int n,m,result=0;
+    static int n, m, result = 0;
+
     public int solution(String[] maps) {
         n = maps.length;
         m = maps[0].length();
         ArrayDeque<int[]> dq = new ArrayDeque<>();
         map = new char[n][m];
         num = new int[n][m];
-        visited = new boolean[n][m];
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<m;j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 map[i][j] = maps[i].charAt(j);
-                if(map[i][j] == 'S') {
-                    dq.offer(new int[]{i,j});
-                    visited[i][j] = true;
+                if (map[i][j] == 'S') {
+                    dq.offer(new int[]{i, j});
+                    num[i][j] = 1;
                 }
             }
         }
-        while(!dq.isEmpty()) {
+        while (!dq.isEmpty()) {
             int[] where = dq.poll();
-            for(int i=0;i<4;i++) {
-                int x = where[0]+dx[i];
-                int y = where[1]+dy[i];
-                if(!check(x,y) || visited[x][y] || map[x][y] == 'X') continue;        
+            for (int i = 0; i < 4; i++) {
+                int x = where[0] + dx[i];
+                int y = where[1] + dy[i];
+                if (!check(x, y) || num[x][y] != 0 || map[x][y] == 'X') continue;
                 num[x][y] = num[where[0]][where[1]] + 1;
-                System.out.println(""+x+"와"+y+"는" + num[x][y]);
-                if(map[x][y] == 'L') {
+                if (map[x][y] == 'L') {
                     result += num[x][y];
-                    System.out.println("중간"+result);
                     num = new int[n][m];
-                    visited = new boolean[n][m];
                     dq = new ArrayDeque<>();
-                    visited[x][y] = true;
-                    dq.offer(new int[] {x,y});
+                    num[x][y] = 1;
+                    dq.offer(new int[]{x, y});
                     break;
                 }
-                if(map[x][y] == 'E' && result!=0) {
+                if (map[x][y] == 'E' && result != 0) {
                     result += num[x][y];
                     System.out.print(result);
-                    return result;
+                    return result - 2;
                 }
-                visited[x][y] = true;
-                dq.offer(new int[] {x,y});
+                dq.offer(new int[]{x, y});
             }
         }
-        return -1;     
+        return -1;
     }
-    static boolean check(int x,int y) {
-        if(x>=0 && x< n && y>=0 && y<m) {
+
+    static boolean check(int x, int y) {
+        if (x >= 0 && x < n && y >= 0 && y < m) {
             return true;
         }
         return false;
