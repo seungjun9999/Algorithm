c2dd73dc53ea5694d25e53c10c5cc1bf2a64f628
SJ Choi <117347226+seungjun9999@users.noreply.github.com>
2025-01-31 19:18:41 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 6.42 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전력망을 둘로 나누기 - 86971 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971#) 
 
 ### 성능 요약
 
-메모리: 77.3 MB, 시간: 6.79 ms
+메모리: 84.1 MB, 시간: 6.42 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 31일 19:10:16
+2025년 01월 31일 19:18:38
 
 ### 문제 설명
 
--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -4,45 +4,35 @@ class Solution {
     public int solution(int n, int[][] wires) {
         int answer = Integer.MAX_VALUE;
         ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-        for(int i=0;i<=n;i++) {
+        for (int i = 0; i <= n; i++) {
             list.add(new ArrayList<>());
         }
-        for(int[] wire : wires) {
+        for (int[] wire : wires) {
             list.get(wire[0]).add(wire[1]);
             list.get(wire[1]).add(wire[0]);
         }
-        for(int[] wire : wires) {
+        for (int[] wire : wires) {
             int q = wire[0];
             int w = wire[1];
-            boolean[] visited = new boolean[n+1];
+            int cnt = 1;
+            boolean[] visited = new boolean[n + 1];
             visited[1] = true;
             ArrayDeque<Integer> dq = new ArrayDeque<>();
-            for(int a : list.get(1)) {
-                if((1==q && a==w) || (1==w && a==q)) continue;
-                visited[a] = true;
-                dq.offer(a);
-            }
-            while(!dq.isEmpty()) {
+
+            dq.offer(1);
+
+            while (!dq.isEmpty()) {
                 int c = dq.poll();
-                for(int a : list.get(c)) {
-                    if(visited[a] || (c==q && a==w || c==w && a==q)) continue;
-                        visited[a] = true;
-                        dq.offer(a);
-                    
+                for (int a : list.get(c)) {
+                    if (visited[a] || (c == q && a == w || c == w && a == q)) continue;
+                    visited[a] = true;
+                    dq.offer(a);
+                    cnt++;
                 }
             }
-            int cnt1=0;
-            int cnt2=0;
-            for(int i=1;i<=n;i++){
-                if(visited[i]){
-                    cnt1++;
-                }else{
-                    cnt2++;
-                }
-            }
-            answer = Math.min(answer, Math.abs(cnt1-cnt2));
+            answer = Math.min(answer, Math.abs(n - cnt * 2));
         }
-        
+
         return answer;
     }
 }
