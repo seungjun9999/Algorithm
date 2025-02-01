028a2c834f293d9fc245a2dab3b4c6b3c7ec9a7a
csjcozy <117347226+seungjun9999@users.noreply.github.com>
2025-02-02 00:09:39 +0900

    추억 점수

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        HashMap<String,Integer> hm = new HashMap<>();
-        for(int i=0;i<name.length;i++) {
-            hm.put(name[i],yearning[i]);
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (int i = 0; i < name.length; i++) {
+            hm.put(name[i], yearning[i]);
         }
         int[] result = new int[photo.length];
-        for(int j=0;j<photo.length;j++) {
-            for(int i=0;i<photo[j].length;i++) {
-                result[j] += hm.getOrDefault(photo[j][i],0);
+        for (int j = 0; j < photo.length; j++) {
+            for (int i = 0; i < photo[j].length; i++) {
+                result[j] += hm.getOrDefault(photo[j][i], 0);
             }
         }
         return result;
