8cb0f3e2bfa4f67a2e767784dd74f095e8178caa
SJ Choi <117347226+seungjun9999@users.noreply.github.com>
2025-02-03 19:54:14 +0900

    [level 3] Title: 순위, Time: 24.93 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -2,53 +2,49 @@ import java.util.*;
 
 class Solution {
     static int n;
-    static ArrayList<ArrayList<Integer>> lose = new ArrayList<>();
-    static ArrayList<ArrayList<Integer>> win = new ArrayList<>();
     public int solution(int n, int[][] results) {
         int answer = 0;
         this.n = n;
-        
-        for(int i=0;i<=n;i++){
+        ArrayList<ArrayList<Integer>> lose = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> win = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
             lose.add(new ArrayList<>());
             win.add(new ArrayList<>());
         }
-        for(int[] result : results) {
+        for (int[] result : results) {
             win.get(result[0]).add(result[1]);
             lose.get(result[1]).add(result[0]);
         }
-        for(int i=1;i<=n;i++){
-            bfs(win,i);
-            bfs(lose,i);
+        for (int i = 1; i <= n; i++) {
+            bfs(win, i);
+            bfs(lose, i);
         }
-        for(int i=0;i<win.get(1).size();i++){
-            System.out.println(win.get(1).get(i));
-        }
-        for(int i=1;i<=n;i++){
-            if(win.get(i).size() + lose.get(i).size() == n-1) {
+        for (int i = 1; i <= n; i++) {
+            if (win.get(i).size() + lose.get(i).size() == n - 1) {
                 answer++;
             }
         }
         return answer;
     }
-    void bfs(ArrayList<ArrayList<Integer>> list,int idx){
-        boolean[] visited = new boolean[n+1];
+
+    void bfs(ArrayList<ArrayList<Integer>> list, int idx) {
+        boolean[] visited = new boolean[n + 1];
         ArrayDeque<Integer> dq = new ArrayDeque<>();
         ArrayList<ArrayList<Integer>> temp = new ArrayList<>();
-        for(int i=0;i<=n;i++){
-            temp.add(new ArrayList<>());
-            temp.get(i).addAll(list.get(i));
-        }        
-        dq.offer(idx);
-        visited[idx] = true;
-        while(!dq.isEmpty()) {
+        for (int i = 0; i <= n; i++) {
+            temp.add(new ArrayList<>(list.get(i)));
+        }
+        for (int a : list.get(idx)) {
+            visited[a] = true;
+            dq.offer(a);
+        }
+        while (!dq.isEmpty()) {
             int now = dq.poll();
-            for(int a : temp.get(now)) {
-                if(!visited[a]) {
+            for (int a : temp.get(now)) {
+                if (!visited[a]) {
                     dq.offer(a);
                     visited[a] = true;
-                    if(!list.get(idx).contains(a)){
-                        list.get(idx).add(a);
-                    }      
+                    list.get(idx).add(a);
                 }
             }
         }
