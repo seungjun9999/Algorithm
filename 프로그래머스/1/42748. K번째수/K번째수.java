e7c677b2e7381b1352fcc1eaf7c0f3defccb9526
SJ Choi <117347226+seungjun9999@users.noreply.github.com>
2025-02-07 19:12:17 +0900

    [level 1] Title: K번째수, Time: 0.47 ms, Memory: 92.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        int i=0;
+        for(int[] command : commands) {;
+            int[] temp = Arrays.copyOfRange(array,command[0]-1,command[1]);
+            Arrays.sort(temp);
+            answer[i++] = temp[command[2]-1];
+        }
+        return answer;
+    }
+}
