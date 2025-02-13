007e61077cd600086cbc8e25a76efbf4dceb8e27
csjcozy <117347226+seungjun9999@users.noreply.github.com>
2025-02-13 18:46:16 +0900

    [level 2] Title: 서버 증설 횟수, Time: 3.05 ms, Memory: 69.6 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -1,10 +1,11 @@
 class Solution {
     public int solution(int n) {
-        int[] num = new int[n+1];
-        num[1]=1;
-        num[2]=2;
-        for(int i=3;i<=n;i++) {
-            num[i] = (num[i-1] + num[i-2])%1000000007;
+        int[] num = new int[n + 1];
+        num[1] = 1;
+        num[2] = 2;
+        // 피보나치..?
+        for (int i = 3; i <= n; i++) {
+            num[i] = (num[i - 1] + num[i - 2]) % 1000000007;
         }
         return num[n];
     }
