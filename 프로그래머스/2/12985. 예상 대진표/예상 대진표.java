bff0485991a80e399785e1898a000312d3a68457
csjcozy <117347226+seungjun9999@users.noreply.github.com>
2025-02-16 18:48:38 +0900

    [level 2] Title: 서버 증설 횟수, Time: 3.05 ms, Memory: 69.6 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,17 +1,15 @@
-class Solution
-{
-    public int solution(int n, int a, int b)
-    {
-        int answer=1;
-        while(Math.abs(b-a)!=1 || ceil(a) != ceil(b)) {
+class Solution {
+    public int solution(int n, int a, int b) {
+        int answer = 1;
+        while (Math.abs(b - a) != 1 || ceil(a) != ceil(b)) {
             b = ceil(b);
             a = ceil(a);
             answer++;
         }
         return answer;
     }
-    
-    int ceil(int x){
-        return (int)Math.ceil((double)x/2);
-    } 
+
+    int ceil(int x) {
+        return (int) Math.ceil((double) x / 2);
+    }
 }
