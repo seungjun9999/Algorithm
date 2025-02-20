6f9157894999b6b22ebc82e6130655276f81ed65
SJ Choi <117347226+seungjun9999@users.noreply.github.com>
2025-02-20 17:26:34 +0900

    [level 2] Title: 피보나치 수, Time: 6.51 ms, Memory: 95 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    static int[] num;
+    static int m = 1234567;
+    public int solution(int n) {
+        num = new int[n+1];
+        num[1]=1;
+        return fibo(n);
+    }
+    private int fibo(int n) {
+        if(num[n]!=0) return num[n];
+        fibo(n-1);
+        num[n] = num[n-1]%m + num[n-2]%m;
+        return num[n]%m;
+    }
+}
