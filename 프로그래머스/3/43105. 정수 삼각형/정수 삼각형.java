0cee2bc111738981a4295a9e958ea9103d9faade
SJ Choi <117347226+seungjun9999@users.noreply.github.com>
2025-02-21 17:33:30 +0900

    [level 3] Title: 정수 삼각형, Time: 9.30 ms, Memory: 63.8 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,20 +1,11 @@
 class Solution {
     public int solution(int[][] triangle) {
         int n = triangle.length;
-        int answer = 0;
-        int[][] dp = new int[n][];
-        for(int i=0;i<n;i++) {
-            dp[i] = triangle[i].clone();
-        }
-        for(int i = 0; i < n - 1 ;i++) {
+        for(int i = n-2; i >= 0 ;i--) {
             for(int j=0;j<triangle[i].length; j++) {
-                dp[i+1][j] = Math.max(dp[i][j]+triangle[i+1][j] , dp[i+1][j]);
-                dp[i+1][j+1] = Math.max(dp[i][j]+triangle[i+1][j+1] , dp[i+1][j+1]);
+               triangle[i][j] +=Math.max(triangle[i+1][j], triangle[i+1][j+1]);
             }
         }
-        for(int temp : dp[n-1]) {
-            answer = Math.max(answer,temp);
-        }
-        return answer;
+        return triangle[0][0];
     }
 }
