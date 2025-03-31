d47dfc945b4d09bf76d0f1170d3520e5d0869f60
SJ Choi <117347226+seungjun9999@users.noreply.github.com>
2025-03-31 19:43:17 +0900

    [Gold V] Title: 치킨 배달, Time: 196 ms, Memory: 18452 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,54 +1,57 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int n, m, sum, result = Integer.MAX_VALUE;
-    static int[][] num;
-    static ArrayList<int[]> home = new ArrayList<>();
+    static int n,m,miin = Integer.MAX_VALUE ;
     static ArrayList<int[]> ck = new ArrayList<>();
-    static boolean[] visited;
+    static ArrayList<int[]> home = new ArrayList<>();
+    static ArrayList<int[]> select = new ArrayList<>();
 
-    public static void main(String[] arg) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        num = new int[n][n];
-        for (int i = 0; i < n; i++) {
+        for(int i=0;i<n;i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                num[i][j] = Integer.parseInt(st.nextToken());
-                if (num[i][j] == 1) {
-                    home.add(new int[]{i, j});
-                } else if (num[i][j] == 2) {
-                    ck.add(new int[]{i, j});
-                }
+            for(int j=0;j<n;j++) {
+                int a = Integer.parseInt(st.nextToken());
+                if(a==1) home.add(new int[]{i,j});
+                else if(a==2) ck.add(new int[]{i,j});
             }
         }
-        visited = new boolean[ck.size()];
-        dfs(0, 0);
-        System.out.println(result);
+
+        dfs(0,0);
+        System.out.print(miin);
     }
 
-    static void dfs(int i, int cnt) {
-        if (cnt == m) {
-            sum = 0;
-            for (int j = 0; j < home.size(); j++) {
-                int miin = Integer.MAX_VALUE;
-                for (int k = 0; k < ck.size(); k++) {
-                    if (visited[k]) {
-                        miin = Math.min(miin, Math.abs(ck.get(k)[0] - home.get(j)[0]) + Math.abs(Math.abs(ck.get(k)[1] - Math.abs(home.get(j)[1]))));
-                    }
-                }
-                sum += miin;
-            }
-            result = Math.min(sum, result);
+    static void dfs(int a,int dep) {
+        if(dep==m) {
+            calc();
             return;
         }
-        for (int a = i; a < ck.size(); a++) {
-            visited[a] = true;
-            dfs(a + 1, cnt + 1);
-            visited[a] = false;
+
+        for(int i=a;i<ck.size();i++) {
+            select.add(ck.get(i));
+            dfs(i+1,dep+1);
+            select.remove(select.size()-1);
         }
+    }
+
+    static void calc() {
+        int sum=0;
+        for(int[] h : home) {
+            int r = Integer.MAX_VALUE;
+            for(int[] s : select) {
+                int t = road(h,s);
+                if(r > t) r = t;
+            }
+            sum+=r;
+        }
+        miin = Math.min(miin,sum);
+    }
+
+    static private int road(int[] a,int[] b) {
+        return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
     }
 }
