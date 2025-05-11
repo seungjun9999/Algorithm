b4a5a7d1a4806580c66a28a7f971c5cd2e4ee08d
jxungsxn <117347226+seungjun9999@users.noreply.github.com>
2025-05-11 14:29:37 +0900

    [Gold III] Title: 조삼모사, Time: 4612 ms, Memory: 216696 KB -BaekjoonHub

---
+++ 백준/Gold/1621. 조삼모사/조삼모사.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int k = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int[] num = new int[n + 1];
+        int[] dp = new int[n + 1];
+        ArrayList<Integer> list = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 1; i < k; i++) {
+            dp[i] = num[i] + dp[i - 1];
+        }
+        for (int i = k; i <= n; i++) {
+            int one = dp[i - 1] + num[i];
+            int get = dp[i - k] + c;
+            if (one <= get) {
+                dp[i] = one;
+            } else {
+                dp[i] = get;
+            }
+        }
+        int cnt = n;
+        while (cnt >= 1) {
+            if(dp[cnt] == dp[cnt-1] + num[cnt]) {
+                cnt-=1;
+            }else {
+                cnt-=k;
+                list.add(cnt+1);
+            }
+        }
+        Collections.sort(list);
+        System.out.println(dp[n]);
+        System.out.println(list.size());
+        for (int a : list) {
+            System.out.print(a + " ");
+        }
+    }
+}
