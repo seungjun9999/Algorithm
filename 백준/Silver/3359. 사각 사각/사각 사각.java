a4d336acc0f4ec8f369085c084360c1b30dff434
jxungsxn <117347226+seungjun9999@users.noreply.github.com>
2025-05-13 16:09:44 +0900

    [Silver I] Title: 사각 사각, Time: 116 ms, Memory: 14472 KB -BaekjoonHub

---
+++ 백준/Silver/3359. 사각 사각/사각 사각.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[][] num;
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        num = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            num[i][0] = Integer.parseInt(st.nextToken());
+            num[i][1] = Integer.parseInt(st.nextToken());
+        }
+        int[][] dp = new int[n][2];
+
+        dp[0][0] = num[0][0];
+        dp[0][1] = num[0][1];
+
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = num[i][0] + Math.max(
+                    dp[i-1][0] + Math.abs(num[i][1] - num[i-1][1]),
+                    dp[i-1][1] + Math.abs(num[i][1] - num[i-1][0])
+            );
+
+            dp[i][1] = num[i][1] + Math.max(
+                    dp[i-1][0] + Math.abs(num[i][0] - num[i-1][1]),
+                    dp[i-1][1] + Math.abs(num[i][0] - num[i-1][0])
+            );
+        }
+        System.out.print(Math.max(dp[n - 1][0], dp[n - 1][1]));
+    }
+}
