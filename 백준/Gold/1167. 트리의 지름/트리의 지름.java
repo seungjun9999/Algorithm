f8b9fee786927bd46648b01a116282d2779a6ce6
jxungsxn <117347226+seungjun9999@users.noreply.github.com>
2025-05-14 14:44:33 +0900

    [Gold II] Title: 트리의 지름, Time: 708 ms, Memory: 97712 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static ArrayList<ArrayList<int[]>> list = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        for (int i = 0; i <= n; i++) {
+            list.add(new ArrayList<>());
+        }
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            while (true) {
+                int b = Integer.parseInt(st.nextToken());
+                if (b == -1) {
+                    break;
+                }
+                int c = Integer.parseInt(st.nextToken());
+                list.get(a).add(new int[]{b, c});
+            }
+        }
+        int[] result = bfs(1);
+        int cnt = 2;
+        while (cnt-- > 0) {
+            result = bfs(result[0]);
+        }
+        System.out.print(result[1]);
+    }
+
+    static private int[] bfs(int start) {
+        ArrayDeque<int[]> dq = new ArrayDeque<>();
+        boolean[] visited = new boolean[n + 1];
+        visited[start] = true;
+        dq.offer(new int[]{start, 0});
+        int maax = 0, lng = start;
+        while (!dq.isEmpty()) {
+            int[] now = dq.poll();
+            if (maax < now[1]) {
+                maax = now[1];
+                lng = now[0];
+            }
+            for (int[] v : list.get(now[0])) {
+                if (!visited[v[0]]) {
+                    visited[v[0]] = true;
+                    dq.offer(new int[]{v[0], now[1] + v[1]});
+                }
+            }
+        }
+        return new int[]{lng, maax};
+    }
+}
