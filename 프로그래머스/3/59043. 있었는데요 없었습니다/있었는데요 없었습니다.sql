c5c1f61cf7fbb1805aceedaabf4e85c5be5b5f91
jxungsxn <117347226+seungjun9999@users.noreply.github.com>
2025-05-15 17:08:16 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,5 +1,5 @@
-SELECT i.animal_id, i.name
-from animal_ins i
-join animal_outs o on i.animal_id = o.animal_id
-where i.datetime > o.datetime
-order by i.datetime asc
+SELECT I.ANIMAL_ID	,I.NAME
+FROM ANIMAL_INS I 
+JOIN ANIMAL_OUTS O ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE I.DATETIME > O.DATETIME
+ORDER BY I.DATETIME
